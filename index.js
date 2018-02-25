afb171aa8ac67e35f3bc2096465a352276e32cdd
Paul Esch-Laurent <p@uly.io>
2018-02-25 01:37:18 -0500

    WIP

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const uuidv4   = require("uuid/v4");
 const fs       = require("fs");
 const { exec } = require("child_process");
 
+// User Modules
+const rpn      = require("./lib/rpn");
+
 // Variables
 const uuid     = uuidv4();
 
@@ -28,10 +31,32 @@ exec(`./camera.py ${uuid}`, (err, stdout, stderr) => {
       if (text && text.text)
         console.log(text.text);
       fs.writeFileSync(`${__dirname}/responses/${uuid}.json`, JSON.stringify(text, null, 2));
+      const normalized = text.text.split("\n").map(n => normalize(n));
+      normalized.forEach(n => console.log(n));
+      const formatted  = normalized.map(f => format(f));
+      formatted.forEach(f => console.log(f));
     })
     .catch(err => {
       console.error("ERROR:", err);
     });
 });
 
+const normalize = input => input.trim().replace(/[^\d+\-\/*%]/g, "");
 
+function format(input) {
+  let chars = input.split(""),
+      acc   = "",
+      expr  = [];
+
+  chars.forEach(c => {
+    if (c == +c) {
+      acc += c;
+    } else {
+      expr.push(acc);
+      expr.push(c);
+      acc = "";
+    }
+  });
+
+  return expr.join(" ");
+}
